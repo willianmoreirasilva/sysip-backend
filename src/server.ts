b6bb9c1443d1dd5b1c62707cb7f0b0d0127c0e84
willianmoreirasilva <willian3285@gmail.com>
2024-03-27 20:58:58 -0400

    create server.ts and install prisma

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import https from 'https';
+import http from 'http';
+import siteRoutes from './routes/site';
+import { requestIntercepter } from './utils/requestintercepter';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.all('*', requestIntercepter);
+
+//app.use('/admin',adminRoutes);
+app.use('/', siteRoutes);
+
+const runServer = (port: number, server: http.Server) => {
+    server.listen(port, ()=> {
+        console.log(`🚀️ Running at PORT ${port}`);
+    })
+}
+
+const devServer = http.createServer(app);
+if(process.env.NODE_ENV === 'production'){
+    //TODO: configurar SSL
+    //TODO: rodar server na 80 e na 443
+}else{
+    const serverPort: number = process.env.PORT? parseInt(process.env.PORT) : 9000;
+    runServer(serverPort, devServer);
+}
+
